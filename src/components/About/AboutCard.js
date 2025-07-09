b27be1e4e1a73c014707d63ab9c517fd8e9d4c1a
Chihab Mezrigui <99685119+chihebmezrigui1@users.noreply.github.com>
2025-07-10 00:51:55 +0200

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -10,11 +10,11 @@ function AboutCard() {
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Chihab MEZRIGUI </span>
             <br />
-            a Master’s student in Data Science at IPSSI Paris .
+            a Master’s student in BIG DATA & IA at IPSSI Paris .
             <br />
             I’m currently in my final year (Master 2) and on track to graduate this year.
             <br />
-            Alongside my studies , I work in alternance at RDIUP, gaining hands-on experience while applying my skills in real-world projects.             
+            Alongside my studies , I work in alternance at RDIUP, gaining hands-on experience while applying my skills in european projects.             
             <br />
             <br/>
             Apart from coding, some other activities that I love to do!
